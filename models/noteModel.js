c3071c59036d8e4848202b3f3333adcdbf6d5523
fahmyfauzi <fahmyfauzii@gmail.com>
2023-09-17 20:56:45 +0700

    update note model

--- models/noteModel.js
+++ models/noteModel.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 // create new schema database
-const note = new Schema({
-    title: String,
-    tags: [String],
-    body: String
-}, {
-    timestamps: true
-});
+const note = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    body: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Note", note);
